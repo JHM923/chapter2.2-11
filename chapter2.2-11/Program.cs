84c346abd720729ac87768009b45bd8ccd8dfacc
username <1151555861@qq.com>
2021-06-28 18:02:51 +0800

    first commit

---
+++ chapter2.2-11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace chapter2._2_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static IEnumerable<int> CreateSimpleIterator()
+            {
+                yield return 10;
+                for (int i = 0; i < 3; i++)
+                {
+                    yield return i;
+                }
+                yield return 20;
+            }
+            foreach (int value in CreateSimpleIterator())
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
